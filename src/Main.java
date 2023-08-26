1021b5750505ec0eac896208f489a84ee2cd119e
Neto test <demon_4@mail.ru>
2023-08-26 11:46:11 +0300

    Первый коммит

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int customerAccount = 100;
+        int replenishmentAmount = 800;
+        int bonus = 0;
+
+        if (replenishmentAmount > 1000) {
+            bonus = replenishmentAmount / 100;
+        }
+
+        int finalBalance = customerAccount+replenishmentAmount+bonus;
+        System.out.println("Итоговый баланс=" +finalBalance);
+    }
+}
